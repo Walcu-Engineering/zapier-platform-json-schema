fb6be5e47d0cf782f511528aea55433aaa243e8e
Jose G.L <jgl@mailbox.org>
2021-07-01 11:30:01 +0200

    Use transformOneOf in ZapierSchemaGenerator

--- src/ZapierSchemaGenerator.ts
+++ src/ZapierSchemaGenerator.ts
@@ -98,7 +98,7 @@ export default class ZapierSchemaGenerator {
     } else if (prop.anyOf) {
       return transformAnyOf(fieldSchema, prop, this);
     } else if (prop.oneOf) {
-      return transformAnyOf(fieldSchema, prop, this);
+      return transformOneOf(fieldSchema, prop, this);
     } else if (prop.allOf) {
       return transformAllOf(fieldSchema, prop, this);
     } else if (!prop.type) {
