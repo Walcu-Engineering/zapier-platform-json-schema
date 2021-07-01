87db4617e9c2d400e078772d75f0fa882fa01412
Jose G.L <jgl@mailbox.org>
2021-07-01 11:40:28 +0200

    Add build for current fix

--- build/transforms/transformOneOf.js
+++ build/transforms/transformOneOf.js
@@ -7,14 +7,10 @@ function transformOneOf(fieldSchema, prop, generator) {
         throw new Error(`Invalid state needs key ${JSON.stringify(prop)}`);
     }
     let typeToParse = prop.oneOf.filter((item) => item.type !== null && item.type !== "null");
-    if (typeToParse.length > 1) {
+    if (typeToParse.length >= 1) {
         typeToParse = typeToParse.filter((item) => item.type !== "string" || item.format);
     }
     if (typeToParse.length === 0) {
-        return null;
-    }
-    if (typeToParse.length > 1) {
-        console.warn(`Skipped prop because of multiple complex types: ${JSON.stringify(prop)}`);
         return null;
     }
     return generator.getFieldSchema(typeToParse.pop(), fieldSchema.key || "unknown");
